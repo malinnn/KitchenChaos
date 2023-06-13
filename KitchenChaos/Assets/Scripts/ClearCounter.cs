91b18c83cc7a9ce40e405a0862bf0079c18dbf2d
Malin <pappmalin@gmail.com>
2023-06-13 14:08:03 +0300

    Implemented moving food from a clear counter to another

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-    [SerializeField] private KitchenObjectSO _kitchenObject;
+    [SerializeField] private KitchenObjectSO _kitchenObjectSO;
     [SerializeField] private GameObject _counterTopPoint;
+    [SerializeField] private KitchenObject _kitchenObject;
+
+    public bool testing;
+    public ClearCounter secondCounter;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if(_kitchenObject != null)
+            {
+                _kitchenObject.SetCurrentCounter(secondCounter);
+            }
+        }
+    }
 
     public void Interact()
     {
-        Debug.Log("Interacted with : " + transform.gameObject.name);
-        Transform kitchenObjectTransform = Instantiate(_kitchenObject.prefab, _counterTopPoint.transform).transform;
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if (_kitchenObject == null)
+        {
+            GameObject kitchenObjectTransform = Instantiate(_kitchenObjectSO.prefab, _counterTopPoint.transform);
+            kitchenObjectTransform.transform.localPosition = Vector3.zero;
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObject());
+            _kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            _kitchenObject.SetCurrentCounter(this);
+        }
+        else
+        {
+            Debug.Log("Food already on : " + _kitchenObject.GetCurrentCounter());
+        }
+    }
+
+    public Transform GetCounterTopPoint()
+    {
+        return _counterTopPoint.transform;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return _kitchenObject;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this._kitchenObject = kitchenObject;
     }
 }
--- KitchenChaos/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos/Assets/Scripts/KitchenObject.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO _kitchenObjectSO;
+    [SerializeField] private ClearCounter _currentCounter;
 
     public KitchenObjectSO GetKitchenObject()
     {
         return _kitchenObjectSO;
+    }
+
+    public ClearCounter GetCurrentCounter()
+    {
+        return _currentCounter;
+    }
+
+    public void SetCurrentCounter(ClearCounter clearCounter)
+    {
+        if (_currentCounter != null)
+        {
+            _currentCounter.SetKitchenObject(null);
+        }
+        _currentCounter = clearCounter;
+        transform.parent = clearCounter.GetCounterTopPoint();
+        clearCounter.SetKitchenObject(this);
+        transform.localPosition = Vector3.zero;
     }
 }
