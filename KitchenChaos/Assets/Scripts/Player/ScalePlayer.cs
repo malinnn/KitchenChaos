1b84a47290d48394094789eb886d5a5a2158428f
Malin <pappmalin@gmail.com>
2024-01-08 12:46:24 +0200

    Modify the ScalePlayer script for deformation and rotation

--- KitchenChaos/Assets/Scripts/Player/ScalePlayer.cs
+++ KitchenChaos/Assets/Scripts/Player/ScalePlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScalePlayer : MonoBehaviour
 {
     [SerializeField] private float scaleOffset = 0.3f;
+    [SerializeField] private float deformOffset = 0.15f;
 
     // Update is called once per frame
     void Update()
@@ -25,6 +26,21 @@ public class ScalePlayer : MonoBehaviour
                 transform.localScale.y - scaleOffset,
                 transform.localScale.z - scaleOffset
             );
+        }
+
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            transform.Rotate(new Vector3(0, 90, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            transform.localScale += new Vector3(deformOffset, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            transform.localScale -= new Vector3(deformOffset, 0, 0);
         }
     }
 }
